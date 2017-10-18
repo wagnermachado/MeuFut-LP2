98e3a48d6f1706e16cc995bb1aa1af9982a4fe88
KibaNaoParsa <elyasnog2@hotmail.com>
2017-10-18 10:15:21 -0200

    Placar ok

--- app/src/main/java/com/example/alunos/meufut/Cronometro.java
+++ app/src/main/java/com/example/alunos/meufut/Cronometro.java
@@ -16,16 +16,39 @@ public class Cronometro extends AppCompatActivity{
 
 
     private long lastPause;
+
+    Button gol1;
+    Button gol2;
+    Button cancela1;
+    Button cancela2;
+    Button pla1;
+    Button pla2;
+
+    int gols1;
+    int gols2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cronometro);
 
+        gols1 = 0;
+        gols2 = 0;
+
         final Chronometer relogio = (Chronometer) findViewById(R.id.relogio);
         Button iniciar = (Button) findViewById(R.id.btnStart);
         Button zerar = (Button) findViewById (R.id.btnBlank);
         Button parar = (Button) findViewById(R.id.btnStop);
+
+        gol1 = (Button) findViewById(R.id.btnGol1);
+        gol2 = (Button) findViewById(R.id.btnGol2);
+        cancela1 = (Button) findViewById(R.id.btnCancela1);
+        cancela2 = (Button) findViewById(R.id.btnCancela2);
+        pla1 = (Button) findViewById(R.id.plaTime1);
+        pla2 = (Button) findViewById(R.id.plaTime2);
+
+
         relogio.setBase(SystemClock.elapsedRealtime());
 
 
@@ -44,6 +67,41 @@ public class Cronometro extends AppCompatActivity{
             public void onClick(View v) { relogio.setBase(SystemClock.elapsedRealtime()); relogio.stop(); lastPause = SystemClock.elapsedRealtime();
             }
         });
+
+        gol1.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                gols1 += 1;
+                pla1.setText(String.valueOf(gols1));
+            }
+        });
+
+        gol2.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                gols2 += 1;
+                pla2.setText(String.valueOf(gols2));
+            }
+        });
+
+        cancela1.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                if (gols1 > 0) {
+                    gols1 -= 1;
+                    pla1.setText(String.valueOf(gols1));
+                }
+            }
+        });
+
+        cancela2.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                if (gols2 > 0) {
+                    gols2 -= 1;
+                    pla2.setText(String.valueOf(gols2));
+                }
+            }
+        });
+
+
+
 
     }
 
